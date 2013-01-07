1315d137dd2bed4e1bc2eebfd09f60c0622a7370
Jeff Hungerford <hungerf3@house.ofdoom.com>
2013-01-06 19:31:58 -0800

    Use a temporary space rather than the current directory.

--- stitch.sh
+++ stitch.sh
@@ -1,8 +1,11 @@
 #! /bin/sh
 
+WORKSPACE=${TMP-/var/tmp/}$$/
+mkdir ${WORKSPACE}
+
 for F in *.jpg;
 do
-    jpegtopnm < ${F} > $(basename ${F} jpg)pnm
+    jpegtopnm < ${F} > ${WORKSPACE}$(basename ${F} jpg)pnm
 done
 
 WIDTH=$(ls -1 *.jpg | cut -f1 -d. | cut -f2 -d_ | sort -n | tail -1)
@@ -13,14 +16,17 @@ do
     WORK= " "
     for COL in $(seq 0 ${WIDTH});
     do
-	WORK="${WORK} ${ROW}_${COL}.pnm"
+	WORK="${WORK} ${WORKSPACE}${ROW}_${COL}.pnm"
     done
-    pnmcat -tb ${WORK} > row-${ROW}.pnm
+    pnmcat -tb ${WORK} > ${WORKSPACE}row-${ROW}.pnm
 done
 WORK= " "
 for ROW in $(seq 0 ${HEIGHT});
 do
-    WORK="${WORK} row-${ROW}.pnm"
+    WORK="${WORK} ${WORKSPACE}row-${ROW}.pnm"
 done
 
-pnmcat -lr ${WORK} > out.pnm
+pnmcat -lr ${WORKSPACE}${WORK} > out.pnm
+
+rm ${WORKSPACE}/*.pnm
+rmdir ${WORKSPACE}
